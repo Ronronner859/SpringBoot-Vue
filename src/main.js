b1c7c5ed350772731ceab638229d8c6e20db4bce
度鲸 <2863519726@qq.com>
2022-11-11 22:37:48 +0800

    vue-cli1.0

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//该文件是整个项目的入口文件
+
+//引入vue这个包 得到VUE 构造函数
 import Vue from 'vue'
+//引入app组件 讲把APP.vue中的模板构造 渲染到HTML页面中
 import App from './App'
+
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// 创建Vue的实例对象
 new Vue({
   el: '#app',
   router,
